d5ade13af4638275a7a23f8494858cfc25bd3cd2
Arshad-Shamim <arshadshamim555@gmail.com>
2025-03-20 17:04:57 +0530

    set app and main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
 
+import {BrowserRouter} from 'react-router-dom';    //for routing..
+import 'bootstrap/dist/css/bootstrap.min.css';    //after installing npm i bootstarp;
+import "bootstrap/dist/js/bootstrap.bundle.min.js"; // For JavaScript
+
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  //copy content
+  <StrictMode>                            
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 )
+
+
