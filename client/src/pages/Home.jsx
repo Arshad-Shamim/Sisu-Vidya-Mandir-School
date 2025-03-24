5d2f0742eac67d9aaaad2d3cc454f985ad2681e0
Arshad-Shamim <arshadshamim555@gmail.com>
2025-03-24 15:09:24 +0530

    except footer all

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -8,9 +8,10 @@ import sportDay from '../assets/sportDay.jpg'
 import annualFuncation from '../assets/annualFuncation.jpg'
 import scienceFair from '../assets/scienceFair.jpeg'
 import independenceDay from '../assets/independenceDay.jpg'
-import hunPer from '../assets/100per.jpg'
-import alumni from '../assets/alumni.webp'
-import competitions from '../assets/competitions.jpg'
+import selfDefence from '../assets/selfDefence.avif'
+import musicDance from '../assets/musicDance.jpg'
+import DigitalClasses from '../assets/DigitalClasses.jpg'
+import scoutTraning from '../assets/scoutTraning.jpg'
 
 export default function Home() {
 
@@ -33,31 +34,33 @@ export default function Home() {
         { title: "Syllabus 2025", link: "https://drive.google.com/file/d/1lTATmmBEW53SQ6v9YsjFV3mYqK4dUoVl/view?usp=drive_link" },
     ];
 
-    const images = [
-        { src: sportDay, alt: "Sports Day" },
-        { src: annualFuncation, alt: "Annual Function" },
-        { src: scienceFair, alt: "Science Fair" },
-        { src: independenceDay, alt: "Independence Day Celebration" },
-    ];
+    // const images = [
+    //     { src: sportDay, alt: "Sports Day" },
+    //     { src: annualFuncation, alt: "Annual Function" },
+    //     { src: scienceFair, alt: "Science Fair" },
+    //     { src: independenceDay, alt: "Independence Day Celebration" },
+    // ];
 
     const achievementsData = [
         {
-          title: "Board Results",
-          year: "2024",
-          description: "100% pass results in board exams consistently",
-          image: hunPer
+          title: "Karate & Self-Defense Training",
+          description: "Building confidence & self-discipline.",
+          image: selfDefence
         },
         {
-          title: "Olympiads & Competitions",
-          year: "2024",
-          description: "Students ranked among top positions in district-level Olympiads & competitions.",
-          image: competitions
+          title: "Digital Learning & Coding Classes",
+          description: "C, HTML, CSS, JavaScript & AI basics.",
+          image: DigitalClasses
         },
         {
-          title: "Alumni Excelling",
-          year: "2024",
-          description: "Successful alumni excelling in various professional fields.",
-          image: alumni
+          title: "Scout Training",
+          description: "Teamwork & adventure programs.",
+          image: scoutTraning
+        },
+        {
+            title: "Music, Dance & Arts",
+            description: "Encouraging creativity & cultural awareness.",
+            image:musicDance
         }
     ];
 
@@ -65,7 +68,7 @@ export default function Home() {
         "Admissions Open for 2025-26 – Secure your child’s future! Apply Now",
         "Science Olympiad Registration Begins – Encourage your child to participate!",
         "Parent-Teacher Meeting (PTM) Notification – Check the schedule for your child’s class.",
-        " Winter Vacation Dates Announced – School will remain closed from [Start Date] to [End Date].",
+        " Winter Vacation Dates Announced – School will remain closed from 15/12/2025 to 31/12/2025",
         " Scholarship Exam for Meritorious Students – Apply before the deadline!"
     ]
       
@@ -73,6 +76,7 @@ export default function Home() {
 
   return (
     <>
+        {/* section1 */}
         <section>
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <div class="container-fluid">
@@ -97,7 +101,8 @@ export default function Home() {
                 </div>
             </nav>
         </section>
-
+        
+        {/* section2 */}
         <section className="video-section">
             <video autoPlay muted loop className="background-video">
                 <source src={bg_viedo} type="video/mp4"/>
@@ -114,67 +119,43 @@ export default function Home() {
                     <h1 className='mx-auto text-center'>Where Education Meet <span className='text-warning dancing-script-text'>&quot;{words[index]}&quot;</span></h1>
                 </div>
         </section>
-
+        
+        {/* section3 */}
         <section id="school-overview">
             <div class="container">
                 <h2 className='heading-color heading'>Welcome to SV Mandir School</h2>
-
+                <p className='heading-color fst-italic'> A Legacy of Excellence in Education</p>
                 <div className='conatiner overview-lg my-3'>
                     <h3 className='sub-heading-color'>About Us</h3>
                     <p>Established with the vision to nurture responsible, disciplined, and knowledgeable citizens, Sisu Vidya Mandir focuses on academic excellence, personality development, and cultural heritage. With a team of dedicated educators, state-of-the-art facilities, and an engaging curriculum, we prepare students to succeed in all walks of life.</p>
                 </div>
 
                 <div className='conatiner overview-lg my-2'>
-                    <h3 className='sub-heading-color'>Our History</h3>
-                    <p>Sisu Vidya Mandir, Hindol, has been a pillar of academic and cultural excellence in Dhenkanal for many years. Founded with the vision of creating a generation of intellectually and morally strong students, the school has grown into a leading educational institution, offering modern facilities, a well-structured curriculum, and a strong emphasis on traditional Indian education</p>
+                    <h3 className='sub-heading-color'>Why Choose Us?</h3>
+                    <ul className='text-start'>
+                            <li>Quality Education: CBSE-aligned syllabus with interactive learning.</li>
+                            <li>Moral & Ethical Values: Strong emphasis on Indian traditions & discipline.</li>
+                            <li>Skill-Based Learning: Digital education, leadership programs & extracurriculars.</li>
+                            <li>Self-Defense & Fitness: Karate, Yoga, and Physical Training programs.</li>
+                            <li>Technology-Enabled Classrooms: Smart classes & e-learning modules.</li>
+                            <li>Scout & Guide Training: Leadership, teamwork & adventure-based learning.</li>
+                            <li>Experienced Faculty: Highly qualified teachers dedicated to student success.</li>
+                    </ul>
                 </div>
 
                 <div className='conatiner overview-lg my-2'>
-                    <h3 className='sub-heading-color'>Our Mission</h3>
+                    <h3 className='sub-heading-color'>Our Academic Approach</h3>
                     <ul className='text-start'>
-                            <li>To provide high-quality education rooted in Indian values and modern learning techniques.</li>
-                            <li>To develop well-rounded individuals by integrating academics, extracurriculars, and life skill</li>
-                            <li>To instill moral and ethical values through discipline, spiritual teachings, and social responsibility.</li>
-                            <li>To encourage innovation and technology by offering digital education and programming skills.</li>
-                            <li>To promote leadership and teamwork through Scout & Guide training and outdoor learning programs.</li>
+                            <li>Innovative Curriculum: Encouraging creative thinking & problem-solving.</li>
+                            <li>Activity-Based Learning: Hands-on projects, science experiments & field trips.</li>
+                            <li>Competitive Exam Preparation: Olympiads, NTSE, and career guidance.</li>
+                            <li>Special Focus on Languages: English, Odia, Hindi, and Sanskrit.</li>
                     </ul>
-                </div>
-                <div class="overview-grid">
-                    <div class="overview-item">
-                    <h3 className='sub-heading-color'>Our Core Value</h3>
-                        <ul className='text-start'>
-                            <li>Discipline & Respect</li>
-                            <li>Academic Excellence</li>
-                            <li>Holistic Development</li>
-                            <li>Innovation & Creativity</li>
-                            <li>Community Engagement</li>
-                        </ul>
-                    </div>
-                    <div class="overview-item">
-                        <h3 className='sub-heading-color'>Facilities</h3>
-                        <ul className='text-start'>
-                            <li>Science & Computer Labs</li>
-                            <li>Sports & Cultural Activities</li>
-                            <li>Library & Digital Learning</li>
-                            <li>Digital Classrooms</li>
-                            <li>Scout & Guide Training</li>
-                        </ul>
-                    </div>
-                    <div class="overview-item">
-                        <h3 className='sub-heading-color'>Why Choose Us?</h3>
-                        <ul className='text-start ps-4'>
-                            <li>Quality Education</li>
-                            <li>Moral & Ethical Values</li>
-                            <li>Skill-Based Learning</li>
-                            <li>Self-Defense & Fitness</li>
-                            <li>Experienced Faculty</li>
-                        </ul>
-                    </div>
                 </div>
             </div>
         </section>
 
-
+        {/* section4 */}
         <section className="container my-4">
             <div className="card shadow-lg">
                 <div className="card-header bg-primary text-white">
@@ -195,8 +176,29 @@ export default function Home() {
             </div>
         </section>
         
+        {/* section5 */}
         <section>
-            <div className="container mt-4">
+            <Container className="my-5">
+                <h2 className="text-center mb-4 heading heading-color">Skill Development Programs</h2>
+                <Row className="justify-content-center">
+                    {achievementsData.map((achievement, index) => (
+                    <Col md={4} sm={6} key={index} className="mb-4">
+                        <Card className="shadow-lg" style={{borderBottom:"2px solid rgba(56, 56, 247, 0.39)"}}>
+                        <Card.Img variant="top" src={achievement.image} alt={achievement.title} style={{height:"35vh"}}/>
+                        <Card.Body style={{height:"20vh"}}>
+                            <Card.Title style={{fontWeight:600}}>{achievement.title}</Card.Title>
+                            <Card.Text className='mt-2'>{achievement.description}</Card.Text>
+                        </Card.Body>
+                        </Card>
+                    </Col>
+                    ))}
+                </Row>
+            </Container>
+        </section>
+        
+        {/* image Gallery */}
+        {/* <section className='my-5'>
+            <div className="container">
                 <h3 className="text-center heading py-1 heading-color">School Activities</h3>
                 <Carousel>
                     {images.map((image, index) => (
@@ -209,27 +211,7 @@ export default function Home() {
                     ))}
                 </Carousel>
             </div>
-        </section>
-
-        <section>
-            <Container className="my-5">
-                <h2 className="text-center mb-4 heading heading-color">Our Achievements</h2>
-                <Row className="justify-content-center">
-                    {achievementsData.map((achievement, index) => (
-                    <Col md={4} sm={6} key={index} className="mb-4">
-                        <Card className="shadow-lg" style={{borderBottom:"2px solid rgba(56, 56, 247, 0.39)"}}>
-                        <Card.Img variant="top" src={achievement.image} alt={achievement.title} style={{height:"35vh"}}/>
-                        <Card.Body>
-                            <Card.Title style={{fontWeight:600}}>{achievement.title}</Card.Title>
-                            <Card.Subtitle className="mb-2 text-muted">{achievement.year}</Card.Subtitle>
-                            <Card.Text style={{height:"15vh",overflowY:"scroll"}}>{achievement.description}</Card.Text>
-                        </Card.Body>
-                        </Card>
-                    </Col>
-                    ))}
-                </Row>
-            </Container>
-        </section>
+        </section> */}
     </>   
   )
 }
